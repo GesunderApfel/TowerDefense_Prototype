4a162dab2197605d944fab320e021ffee9d28397
Denis Schlusche <denis.schlusche@web.de>
2025-04-22 23:12:43 +0200

    ally refactoring (WIP)

--- scripts/combat/combat_ally.gd
+++ scripts/combat/combat_ally.gd
@@ -1,31 +1,14 @@
 extends CharacterBody2D
 
-enum CombatMovementType {
-	GROUNDED, 
-	FLYING,
-}
-
-enum AllyState {
-	DEFEND_CARRIAGE,
-	ATTACK_TARGET,
-	DYING,
-}
-
-enum AnimationTreeState {
-	IDLE,
-	WALKING,
-	ATTACKING,
-	DYING
-}
-
-
-var current_state := AllyState.DEFEND_CARRIAGE
+@onready var state_chart = $StateChart
 
 var body2D : Node = self
 var carriage : Node
 var target : Node
 
 @export var carriage_defense_radius := 200.0
+
+@onready var combat_health_bar = $CombatHealthBar
 
 # members
 var health : int
@@ -39,23 +22,21 @@ var move_speed : float = 200
 @onready var sprite = $Visuals/AnimatedSprite2D
 @onready var animation_tree = $Visuals/AnimationTree
 var animation_state_machine : AnimationNodeStateMachinePlayback
-const animate_state_idle = "idle"
-const animate_state_walk = "walk"
-const animate_state_die = "die"
-const animate_state_attack = "attack"
-const animate_state_getHit = "get_hit"
+const anim_state_idle = "idle"
+const anim_state_walk = "walk"
+const anim_state_die = "die"
+const anim_state_attack = "attack"
+const anim_state_getHit = "get_hit"
 
 
 # Physics
 @onready var attack_area_melee_r = $Areas/Areas_FacingRight/AttackArea_Melee_R
 @onready var attack_area_melee_l = $Areas/Areas_FacingLeft/AttackArea_Melee_L
-@onready var area_attack_radius = $Areas/Area_AttackRadius
+@onready var area_attack_radius = $Areas/Area_ScanRadius
 
 # Timers
 var timer_animation_dict : Dictionary = {}
-@onready var timer_wait_after_melee_attack = $Timers/timer_wait_after_melee_attack
-@onready var timer_transition_idle_walk = $Timers/timer_transition_idle_walk
-
+@onready var timer_attack = $Timers/timer_attack
 
 var is_looking_left : bool = false
 
@@ -65,19 +46,19 @@ func _ready():
 	animation_tree.active = true
 		
 	UtilityStateMachine.create_timer_for_animation\
-		(self,animation_tree, timer_animation_dict,animate_state_idle)
+		(self,animation_tree, timer_animation_dict,anim_state_idle)
 		
 	UtilityStateMachine.create_timer_for_animation\
-		(self,animation_tree, timer_animation_dict,animate_state_walk)
+		(self,animation_tree, timer_animation_dict,anim_state_walk)
 	
 	UtilityStateMachine.create_timer_for_animation\
-		(self,animation_tree, timer_animation_dict,animate_state_die)
+		(self,animation_tree, timer_animation_dict,anim_state_die)
 		
 	UtilityStateMachine.create_timer_for_animation\
-		(self,animation_tree, timer_animation_dict,animate_state_getHit)
+		(self,animation_tree, timer_animation_dict,anim_state_getHit)
 		
 	UtilityStateMachine.create_timer_for_animation\
-		(self,animation_tree, timer_animation_dict,animate_state_attack)
+		(self,animation_tree, timer_animation_dict,anim_state_attack)
 		
 		
 	animation_state_machine = UtilityStateMachine.get_playback(animation_tree)
@@ -86,62 +67,17 @@ func _ready():
 func _process(_delta):
 	look_at_target()
 
-func _physics_process(_delta):
-	#print(current_state)
-	match current_state:
-		AllyState.DEFEND_CARRIAGE:
-			state_defend_carriage()
-		AllyState.ATTACK_TARGET:
-			state_attack_target()
-		AllyState.DYING:
-			state_dying()
-	pass
-
 func look_at_target():
 	if(target == null):
 		return
-
+		
 	is_looking_left = position.direction_to(target.position).x < 0
 	sprite.flip_h = is_looking_left
 	pass
 
 
-func state_defend_carriage():
-	for body in area_attack_radius.get_overlapping_bodies():
-		if body.is_in_group("enemy"):
-			target = body
-			current_state = AllyState.ATTACK_TARGET
-			return
-			
-	var carriage_direction : Vector2 = \
-		position.direction_to(carriage.position)
-		
-	var is_carriage_left = carriage_direction.x < 0
-	
-	if position.distance_to(carriage.position) < carriage_defense_radius:
-		animation_tree.current_state = \
-			AnimationTreeState.keys()[AnimationTreeState.IDLE]
-		if is_carriage_left == is_looking_left:
-			is_looking_left = not is_looking_left # turn player in next frame
-	else:
-		is_looking_left = is_carriage_left
-		animation_tree.current_state = \
-			AnimationTreeState.keys()[AnimationTreeState.WALKING]
-		velocity = move_speed * carriage_direction
-		move_and_slide()
-	pass
-
-func state_attack_target():
-	# if attacked, wait half the time to make next decision
-	if timer_animation_dict[AnimationTreeState.WALKING].is_stopped():
-		animation_tree.current_state = \
-			AnimationTreeState.keys()[AnimationTreeState.IDLE]
-			
-	if not timer_wait_after_melee_attack.is_stopped() \
-		and timer_wait_after_melee_attack.time_left > \
-			(timer_wait_after_melee_attack.wait_time / 2):
-				return
-				
+## Could return a list of enemies instead
+func find_enemy():
 	var enemy : Node
 	var nearest_distance = 10000
 	for body in area_attack_radius.get_overlapping_bodies():
@@ -149,55 +85,22 @@ func state_attack_target():
 			if nearest_distance > body.position.distance_to(position):
 				nearest_distance = body.position.distance_to(position)
 				enemy = body
-	
-	# if no enemies are in attack radius -> defend
-	if enemy == null:
-		current_state = AllyState.DEFEND_CARRIAGE
-		return
-	
-	# check if enemy is in hit range
+	return enemy
+
+func is_enemy_in_hit_range(enemy : Node):
 	var area = attack_area_melee_l if is_looking_left else attack_area_melee_r
 	if area.overlaps_body(enemy):
-			if timer_wait_after_melee_attack.is_stopped():
-				timer_wait_after_melee_attack.start()
-				enemy.receive_damage(attack)
-				animation_tree.current_state = \
-					AnimationTreeState.keys()[AnimationTreeState.ATTACKING]
-	else:
-		animation_tree.current_state = \
-			AnimationTreeState.keys()[AnimationTreeState.WALKING]
-		
-		timer_animation_dict[AnimationTreeState.WALKING].start()
-		var direction_to_enemy = position.direction_to(enemy.position)
-		velocity = direction_to_enemy * move_speed
-		is_looking_left = false if direction_to_enemy.x > 0 else true
-		move_and_slide()
-	#	walk to enemy
-		
-	pass
-	
-var already_started_dying = false
-func state_dying():
-	if timer_animation_dict[AnimationTreeState.DYING].is_stopped():
-		if already_started_dying:
-			queue_free()
-		else:
-			timer_animation_dict[AnimationTreeState.DYING].start()
-		pass
-	queue_free()
-	pass
-	
-
-
-
-@onready var combat_health_bar = $CombatHealthBar
+		return true
+	return false
 
 func receive_damage(damage):
 	health -= max(damage-defense,0)
 	combat_health_bar.update_health_value(float(health) / float(max_health) * 100.0)
 	
 	if health <= 0:
-		current_state= AllyState.DYING
+		state_chart.send_event("sce_die")
+	else:
+		state_chart.send_event("sce_get_hit")
 	pass
 
 # ###############################
@@ -205,34 +108,68 @@ func receive_damage(damage):
 # ###############################
 
 func _on_wait_at_carriage_state_physics_processing(_delta):
-	animation_state_machine.travel(animate_state_idle)
+	var enemy : Node = find_enemy()
+	if enemy:
+		target = enemy
+		state_chart.send_event("sce_start_combat")
+		return
 	pass
 
 func _on_return_to_carriage_state_physics_processing(_delta):
-	animation_state_machine.travel(animate_state_walk)
+	var carriage_direction : Vector2 = \
+		position.direction_to(carriage.position)
+		
+	var is_carriage_left = carriage_direction.x < 0
+	
+	if position.distance_to(carriage.position) < carriage_defense_radius:
+		state_chart.send_event("sce_wait")
+		if is_carriage_left == is_looking_left:
+			is_looking_left = not is_looking_left # turn player in next frame
+	else:
+		is_looking_left = is_carriage_left
+		velocity = move_speed * carriage_direction
+		move_and_slide()
 	pass
 
 func _on_move_to_target_state_physics_processing(_delta):
-	animation_state_machine.travel(animate_state_walk)
+	var enemy = find_enemy()
+	if enemy == null:
+		state_chart.send_event("sce_start_defense")
+		return
+	
+	if is_enemy_in_hit_range(enemy):
+		state_chart.send_event("sce_idle")
+		return
+	
+	var direction_to_enemy = position.direction_to(enemy.position)
+	is_looking_left = direction_to_enemy.x < 0
+	velocity = direction_to_enemy * move_speed
+	move_and_slide()
 	pass
 
 func _on_idle_state_physics_processing(_delta):
-	animation_state_machine.travel(animate_state_idle)
+	var enemy = is_enemy_in_hit_range(find_enemy())
+	if enemy:
+		if timer_attack.is_stopped():
+			timer_attack.start()
+			state_chart.send_event("sce_attack")
+	else:
+		state_chart.send_event("sce_move")
 	pass
 
 func _on_attack_state_physics_processing(_delta):
-	animation_state_machine.travel(animate_state_attack)
-	timer_animation_dict[animate_state_attack].start()
+	if timer_animation_dict[anim_state_attack].is_stopped():
+		state_chart.send_event("sce_idle")
 	pass
 
 func _on_get_hit_state_physics_processing(_delta):
-	animation_state_machine.travel(animate_state_getHit)
-	timer_animation_dict[animate_state_getHit].start()
+	if timer_animation_dict[anim_state_getHit].is_stopped():
+		state_chart.send_event("sce_old_state")
 	pass
 
 func _on_die_state_processing(_delta):
-	animation_state_machine.travel(animate_state_die)
-	timer_animation_dict[animate_state_die].start()
+	if timer_animation_dict[anim_state_die].is_stopped():
+		queue_free()
 	pass
 
 # ###############################
@@ -240,25 +177,34 @@ func _on_die_state_processing(_delta):
 # ###############################
 
 func _on_wait_at_carriage_state_entered():
-	
+	animation_state_machine.travel(anim_state_idle)
 	pass # Replace with function body.
 
 func _on_return_to_carriage_state_entered():
+	animation_state_machine.travel(anim_state_walk)
 	pass # Replace with function body.
 
 func _on_move_to_target_state_entered():
+	animation_state_machine.travel(anim_state_walk)
 	pass # Replace with function body.
 
 func _on_idle_state_entered():
+	animation_state_machine.travel(anim_state_idle)
 	pass # Replace with function body.
 
 func _on_attack_state_entered():
+	animation_state_machine.travel(anim_state_attack)
+	timer_animation_dict[anim_state_attack].start()
 	pass # Replace with function body.
 
 func _on_get_hit_state_entered():
+	animation_state_machine.travel(anim_state_getHit)
+	timer_animation_dict[anim_state_getHit].start()
 	pass # Replace with function body.
 
 func _on_die_state_entered():
+	animation_state_machine.travel(anim_state_die)
+	timer_animation_dict[anim_state_die].start()
 	pass # Replace with function body.
 
 
